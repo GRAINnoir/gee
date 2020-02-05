cb7318ac01d8ed2ee4c1c699726ab4f3fff02051
gee <special.geeee@gmail.com>
2020-02-06 00:52:15 +0100

    added ContentSection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import NavList from './Components/NavList';
+import ContentSection from './Components/ContentSection';
 
 const links = ["ABOUT", "AUDIO", "VIDEO", "SOFTWARE", "SKATE TV", "BLOG", "CONTACT"];
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <NavList links={links}/>
+      <ContentSection />
     </div>
   );
 }
---
+++ src/Components/ContentSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/ContentSection.css';
+
+function ContentSection(props) {
+    return (
+        <section className="ContentSection">
+            <div>
+                CONTENT
+            </div>
+        </section>
+    )
+}
+
+export default ContentSection;
