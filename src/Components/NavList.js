d56000fdc8aa8e90561b536a021ad4b8a444791d
gee <special.geeee@gmail.com>
2020-02-07 19:00:22 +0100

    added ListButton Component

--- src/Components/NavList.js
+++ src/Components/NavList.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './styles/NavList.css';
 
-function ListItem(props) {
+function ListButton(props) {
     return (
-        <li className="ListItem"><a href="./">{props.name}</a></li>
+        <li className="ListItem">
+            <button>
+                {props.name}
+            </button>
+        </li>
     )
 }
 
+// function ListItem(props) {
+//     return (
+//         <li className="ListItem">
+//             <a href="./">{props.name}</a>
+//         </li>
+//     )
+// }
+  
 function NavList(props) {
     const links = props.links;
     const listItems = links.map((link) =>
-        <ListItem key={link.toString()} name={link} />
+        // <ListItem key={link.toString()} name={link} />
+        <ListButton key={link.toString()} name={link} />
     );
     return (
         <ul className="NavList">
